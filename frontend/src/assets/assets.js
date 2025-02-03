f76c7ca0b0b864ea1cb2f4321d2fdd984e3e0799
Fv24 <veliufjorlinda@gmail.com>
2025-02-03 15:52:59 +0100

    first-commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,20 @@
+import logo from './logo.png';
+
+export const assets = {
+    logo
+} 
+
+export const products = [
+    {
+        id:"DECO-001",
+        name: "Modern Sofa",
+        description: "Modern design meets ultimate comfort",
+        price: 500,
+        image:[logo],
+        category: "Furniture",
+        subCategory: "Living Room",
+        colors: ["blue","gray","white"],
+        date: 17022025,
+        bestseller:true
+    }
+]
